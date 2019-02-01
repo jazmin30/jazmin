8899b1966fed09d11b5a538e91dbd4c60a4efc00
Juan Christian López López <JLopezL@iingen.unam.mx>
2019-02-01 16:32:00 -0600

    intento

---
+++ js/app.js
@@ -0,0 +1,16 @@
+ $(document).ready(function(){
+    $('.parallax').parallax();
+	$(".dropdown-trigger").dropdown();
+	$('.sidenav').sidenav();
+	 $('.materialboxed').materialbox();
+	 M.updateTextFields();
+	  $('.carousel').carousel();
+	   $('.slider').slider();
+
+});
+
+ $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+
+  });
